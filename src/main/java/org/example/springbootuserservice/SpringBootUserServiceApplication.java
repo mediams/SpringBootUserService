cdb6b7ae7a2b9a62c156da8a9222aa8e657fa5c0
Yevhen Kariev <yevhenkariev@gmail.com>
2025-10-04 12:38:08 +0200

    Project initialization.

---
+++ src/main/java/org/example/springbootuserservice/SpringBootUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootuserservice/SpringBootUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
