7c5aecd5480ed0cdd96d2663fcded11779af2c1c
Yevhen Kariev <yevhenkariev@gmail.com>
2025-10-07 18:17:41 +0200

    Test DB data

--- src/main/java/org/example/springbootuserservice/entity/User.java
+++ src/main/java/org/example/springbootuserservice/entity/User.java
@@ -1,15 +1,13 @@
 package org.example.springbootuserservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
